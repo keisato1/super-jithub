581a8a9b1ec7ba93192b81cee5ebe7a8681aa332
Laryn Qi <qilaryn@gmail.com>
2017-11-29 09:40:45 -0800

    ADDED FILES

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class HelloWorld here.
+* 
+* @author (Kei Sato) 
+* @version (8/17/17)
+*/
+public class HelloWorld
+{
+    public static void main()
+    {
+        System.out.println("Hello World!");
+    }
+}
